2e1971f74c013922a652afd533a8b9859228c4c3
margaretpalma <mpalma1496@gmail.com>
2025-12-15 09:07:06 -0500

    CategoriesController

--- src/main/java/org/yearup/controllers/CategoriesController.java
+++ src/main/java/org/yearup/controllers/CategoriesController.java
@@ -1,13 +1,16 @@
 package org.yearup.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 import org.yearup.data.CategoryDao;
 import org.yearup.data.ProductDao;
 import org.yearup.models.Category;
 import org.yearup.models.Product;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 // add the annotations to make this a REST controller
@@ -31,6 +34,7 @@ public class CategoriesController {
         this.categoryDao = categoryDao;
         this.productDao = productDao;
     }
+
 
     // add the appropriate annotation for a get action
     @GetMapping
