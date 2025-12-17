59f0122dd14c2df2810b48541934250544067e66
margaretpalma <mpalma1496@gmail.com>
2025-12-17 11:57:52 -0500

    ShoppingCartDao

--- src/main/java/org/yearup/data/ShoppingCartDao.java
+++ src/main/java/org/yearup/data/ShoppingCartDao.java
@@ -4,14 +4,10 @@ import org.yearup.models.ShoppingCart;
 
 public interface ShoppingCartDao
 {
-    ShoppingCart getByUserId(int userId);
-    // add additional method signatures here
+    ShoppingCart geByUserId(int userId);
 
     void addItem(int userId, int productId, int quantity);
-
     void updateQuantity(int userId, int productId, int quantity);
-
     void clearCart(int userId);
-
 }
 
