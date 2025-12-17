c05f2402134db945ba6fe6dd111b70116ef968b3
margaretpalma <mpalma1496@gmail.com>
2025-12-17 16:01:35 -0500

    Shopping Cart

--- src/main/java/org/yearup/data/mysql/MySqlShoppingCartDao.java
+++ src/main/java/org/yearup/data/mysql/MySqlShoppingCartDao.java
@@ -2,8 +2,11 @@ package org.yearup.data.mysql;
 import org.springframework.stereotype.Component;
 import org.yearup.data.ShoppingCartDao;
 import org.yearup.models.ShoppingCart;
+import org.yearup.models.ShoppingCartItem;
 
 import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 
 
 @Component
@@ -18,6 +21,21 @@ public class MySqlShoppingCartDao extends MySqlDaoBase implements ShoppingCartDa
     @Override
     public ShoppingCart getByUserId(int userId)
     {
+        ShoppingCart cart = new ShoppingCart();
+
+        //sql query fpr shopping cart update
+        String sql = """
+                UPDATE shopping_cart
+                SET quantity = ?
+                WHERE user_id = ? AND product_id = ? 
+                
+                
+                """;
+
+        //connection to databse
+
+        try(Connection connection = getConnection();
+            PreparedStatement statement = connection.prepareStatement(sql)
 
 
 
